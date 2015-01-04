f2e36052475a094781909334151095f8bcacb9e1
Lukas Rascius <lrascius93@gmail.com>
2015-01-03 19:52:16 -0500

    Added the sleep functionality when in the critical section

--- algorithm.c
+++ algorithm.c
@@ -1,187 +1,146 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
 
-#define CHILD      			0  			/* Return value of child proc from fork call */
-#define TRUE       			0  
-#define FALSE      			1
+#define BUFSIZE 1
+#define PERMS 0666 //0666 - To grant read and write permissions 
 
-FILE *fp1, *fp2, *fp3, *fp4;			/* File Pointers */
+const int N = 6;
+const int loopMax = 200;
+enum Status {IDLE, WANT_IN, IN_CS};
 
-main()
+double elapsed;
+int x = 2;
+int *flag;
+int *turn = 0;
+int *counter = 0;
+clock_t start, stop;
+
+void Process(int i)
 {
-	int pid;						// Process ID after fork call
-	int i;							// Loop index
-	int N;							// Number of times dad does update
-	int N_Att;						// Number of time sons allowed to do update
-	int status;						// Exit status of child process
-	int bal1, bal2;					// Balance read by processes
-	int flag, flag1;				// End of loop variables
-	
-	//Initialize the file balance to be $100
-	fp1 = fopen("balance","w");
-	bal1 = 100;
-	fprintf(fp1, "%d\n", bal1);
-	fclose(fp1);
-	
-	//Initialize the number of attempts to be 20
-	fp4 = fopen("attempt", "w");
-	N_Att = 10;
-	fprintf(fp4, "%d\n", N_Att);
-	fclose(fp4);
-	
-	//Create child processes that will do the updates
-		if ((pid = fork()) == -1) 
-	{
-		//fork failed!
-		perror("fork");
-		exit(1);
-	}
-	
-	if (pid == CHILD){
-	//First Child Process. Dear old dad tries to do some updates.
-	
-		N=5;
-		for(i=1;i<=N; i++)
+	int j, index = 0;
+	printf("Child: %d began running!\n", i);
+	sleep(3);
+	while(index < loopMax)
+	{	
+		while (1)
 		{
-			printf("Dear old dad is trying to do update.\n");
-			fp1 = fopen("balance", "r+");
-			fscanf(fp1, "%d", &bal2);
-			printf("Dear old dad reads balance = %d \n", bal2);
-			
-			//Dad has to think (0-14 sec) if his son is really worth it
-			sleep(rand()%15);
-			fseek(fp1,0L,0);
-			bal2 += 60;
-			printf("Dear old dad writes new balance = %d \n", bal2);
-			fprintf(fp1, "%d \n", bal2);
-			fclose(fp1);
+			*(flag+i) = WANT_IN;
+			j = *turn;
+			while (j != i) 
+			{
 
-			printf("Dear old dad is done doing update. \n");
-			sleep(rand()%5);	/* Go have coffee for 0-4 sec. */
-		}
-	}
-	
-	else
-	{
-		//Parent Process. Fork off another child process.
-		if ((pid = fork()) == -1)
-		{
-			//Fork failed!
-			perror("fork");
-			exit(1);
-		}
-		if (pid == CHILD)
-		{
-			printf("First Son's Pid: %d\n",getpid());
-			//Second child process. First poor son tries to do updates.
-			flag = FALSE;
-			while(flag == FALSE) 
-			{
-				fp3 = fopen("attempt" , "r+");
-				fscanf(fp3, "%d", &N_Att);
-				if(N_Att == 0)
-				{
-					fclose(fp3);
-					flag = TRUE;
+				if (*(flag+j) != IDLE)
+				{ 
+					j = *turn;
 				}
-				else
+				else 
 				{
-					printf("Poor SON_1 wants to withdraw money.\n");
-					fp2 = fopen("balance", "r+");
-					fscanf(fp2,"%d", &bal2);
-					printf("Poor SON_1 reads balance. Available Balance: %d \n", bal2);
-					if (bal2 == 0)
-					{
-						fclose(fp2);
-						fclose(fp3);
-					}
-					else
-					{
-						sleep(rand()%5);
-						fseek(fp2,0L, 0);
-						bal2 -=20;
-						printf("Poor SON_1 write new balance: %d \n", bal2);
-						fprintf(fp2,"%d\n", bal2);
-						fclose(fp2);
-						printf("poor SON_1 done doing update.\n");
-						fseek(fp3,0L, 0);
-						N_Att -=1;
-						fprintf(fp3, "%d\n", N_Att);
-						fclose(fp3);
-					}
+					j = (j + 1) % N;
 				}
 			}
-		}
-		else
-		{
-		//Parent Process. Fork off one more child process.
-			if ((pid = fork()) == -1) 
-			{
-				//fork failed!
-				perror("fork");
-				exit(1);
-			}
-			if (pid == CHILD)
-			{
-				printf("Second Son's Pid: %d\n",getpid());
-				//Third child process. Second poor student tries to do updates.
-				flag1 = FALSE;
-				while(flag1 == FALSE) 
-				{
-					fp3 = fopen("attempt" , "r+");
-					fscanf(fp3, "%d", &N_Att);
-					if(N_Att == 0)
-					{
-						fclose(fp3);
-						flag1 = TRUE;
-					}
-					else
-					{
-						printf("Poor SON_2 wants to withdraw money.\n");
-						fp2 = fopen("balance", "r+");
-						fscanf(fp2,"%d", &bal2);
-						printf("Poor SON_2 reads balance. Available Balance: %d \n", bal2);
-						if (bal2 == 0)
-						{
-							fclose(fp2);
-							fclose(fp3);
-						}
-						else
-						{
-							sleep(rand()%5);
-							fseek(fp2,0L, 0);
-							bal2 -=20;
-							printf("Poor SON_2 write new balance: %d \n", bal2);
-							fprintf(fp2,"%d\n", bal2);
-							fclose(fp2);
 
-							printf("poor SON_2 done doing update.\n");
-							fseek(fp3,0L, 0);
-							N_Att -=1;
-							fprintf(fp3, "%d\n", N_Att);
-							fclose(fp3);
-						}
-					}
-				}
-			}
-			else
+			*(flag+i) = IN_CS;
+			j = 0;
+
+			while ((j < N) && ((j == i) || (*(flag+j) != IN_CS)))
 			{
-				//Now parent process waits for the child processes to finish
-				pid = wait(&status);
-				printf("Process(pid = %d) exited with the status %d. \n", pid, status);
-			
-				pid = wait(&status);
-				printf("Process(pid = %d) exited with the status %d. \n", pid, status);
-			
-				pid = wait(&status);
-				printf("Process(pid = %d) exited with the status %d. \n", pid, status);
+				j = j + 1;
 			}
-			exit(0);
+
+			if((j >= N) && (*turn == i || (*(flag+*turn) == IDLE)))
+			{
+				break;
+			}
 		}
-		exit(0);
+		*turn = i;
+
+		/* CRITICAL SECTION */
+
+		stop = clock();
+		elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+		printf("Process %d took %f milliseconds to enter the CS!\n", i, elapsed);
+		printf("Process %d with index %d\n", i, index+1);
+		printf("Process %d ", i);		
+		printf(" has entered the critical section and went to sleep!\n");
+		sleep(1);
+		printf("Process %d ", i);
+		*counter = *counter + i;
+		printf(" has woke up and updated the counter to %d \n", *counter);
+
+		/* END OF CRITICAL SECTION */
+		start = clock();
+
+		j = (*turn + 1) % N;
+	
+		while (*(flag+j) == IDLE)
+		{			
+			j = (j + 1) % N;
+		}
+			*turn = j;
+			*(flag+i) = IDLE;
+		index++;
 	}
-	exit(0);
 }
 
+int main()
+{
+	int i, pid, shmid;
 
+	/* Initialize Shared Memory */
+	counter = mmap(NULL, sizeof *counter, PROT_READ | PROT_WRITE, 
+                    MAP_SHARED | MAP_ANON, -1, 0);
+	*counter = 0;
+	turn = mmap(NULL, sizeof *turn, PROT_READ | PROT_WRITE, 
+                    MAP_SHARED | MAP_ANON, -1, 0);
+	*turn = 0;
 
+	if((shmid = shmget(1000, BUFSIZE, IPC_CREAT | PERMS)) < 0)
+	{
+		printf("\n unable to create shared memory");
+ 		return 0;
+	}
+
+	/* Initialize Shared Array */
+	if((flag=(int*)shmat(shmid,(char*)0,0)) == (int*)-1)
+	{
+  		printf("\n Shared memory allocation error\n");
+  		exit(1);
+	}
+	
+	/* Initialize flags to IDLE */
+	int loopIndex;
+	for(loopIndex = 0; loopIndex < N; loopIndex++)
+		*(flag+loopIndex) = IDLE;
+
+	int child;
+	for (child = 0; child < N; child++) 
+	{
+    	pid = fork();
+    	if (pid) 
+    	{
+        	continue;
+    	} 
+    	else if (pid == 0) 
+    	{
+    		Process(child);
+        	break;
+    	} 
+    	else 
+    	{
+        	printf("Fork error!\n");
+        	exit(1);
+    	}
+	}
+	
+	return 0;
+}
 
